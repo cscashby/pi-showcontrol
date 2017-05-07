7696b50c3882080fb224d8ff27ff2ca4912cf187
Christian Ashby <git@cashby.me.uk>
2017-05-07 09:24:34 +0100

    Added track name display

--- pi-clicktrack.py
+++ pi-clicktrack.py
@@ -4,6 +4,7 @@ import time
 import signal
 import sys
 import threading
+import json
 import Adafruit_CharLCD as LCD
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
@@ -16,14 +17,19 @@ SERVER_PORT = 53000
 # TODO: Dynamic allocation of IP(s) to listen on
 MY_IP = "192.168.5.108"
 RESPONSE_PORT = 53001
+# - time taken before further keys are activated (debounce time)
+DEBOUNCE_TIME = 0.5
 
 def send_osc(msg):
   client = udp_client.SimpleUDPClient(SERVER_IP, SERVER_PORT)
   client.send_message(msg, [])
 
-def display_handler(unused_addr, args, volume):
+def display_handler(unused_addr, args):
   try:
-    print("[{0}] ~ {1}".format(args[0], args[1](volume)))
+    j = json.loads(args)
+    displayName = j['data']
+    lcd.clear()
+    lcd.message(lcd_text + "\n{name:.16}".format(name=displayName))
   except ValueError: pass
 
 def get_cuename():
@@ -31,7 +37,7 @@ def get_cuename():
 
 def start_server():
   d = dispatcher.Dispatcher()
-  d.map("/reply/cue_id/*/displayName", print)
+  d.map("/reply/cue_id/*/displayName", display_handler)
 
   global server
   server = osc_server.ThreadingOSCUDPServer((MY_IP, RESPONSE_PORT), d)
@@ -51,7 +57,12 @@ def setup():
 
   lcd.set_color(1.0, 1.0, 1.0)
   lcd.clear()
-  lcd.message("Press play")
+  set_message("Press play")
+
+def set_message(text):
+  global lcd_text
+  lcd_text = text
+  lcd.message(text)
 
 if __name__ == "__main__":
   setup()
@@ -62,19 +73,22 @@ if __name__ == "__main__":
     if lcd.is_pressed(LCD.RIGHT):
       # 'Go' button is pressed
       lcd.clear()
-      lcd.message("Playing")
+      set_message("Playing")
       lcd.set_color(0.0, 1.0, 0.0)
       send_osc("/cue/selected/start")
       get_cuename()
+      time.sleep(DEBOUNCE_TIME)
     if lcd.is_pressed(LCD.LEFT):
       # 'Stop' button is pressed
       lcd.clear()
-      lcd.message("Paused")
+      set_message("Paused")
       lcd.set_color(1.0, 0.0, 0.0)
       send_osc("/cue/selected/pause")
       get_cuename()
+      time.sleep(DEBOUNCE_TIME)
     if lcd.is_pressed(LCD.SELECT):
       lcd.set_color(1.0, 1.0, 1.0)
       lcd.clear()
-      lcd.message("Press play")
+      set_message("Press play")
       send_osc("/cue/selected/stop")
+      time.sleep(DEBOUNCE_TIME)
