490b43b3a86f492315625e1c9652c36912d9088e
Christian Ashby <git@cashby.me.uk>
2017-06-14 20:52:45 +0000

    Updated module loading to allow multiple modules, cleaned up naming conventions

--- src/modules/_Module.py
+++ src/modules/_Module.py
@@ -1,5 +1,8 @@
 
 class _Module:
   def __init__(self):
+    #print("Created class {}".format(self.__class__.__name__))
+    # Create thread and start it
+    # Do other stuff here
     pass
   
--- src/showcontrol.py
+++ src/showcontrol.py
@@ -14,34 +14,50 @@ output_threads = []
 
 global importedModules
 importedModules = {}
+global inputThreads
+inputThreads = {}
+global outputThreads
+outputThreads = {}
 
 def signal_handler(signal, frame):
     # Quit all threads cleanly
     os._exit(0)  
 
 def importModule(type, name):
-  f, filename, description = imp.find_module("modules")
-  module = imp.load_module("modules", f, filename, description)
-  f, filename, description = imp.find_module(type, [filename])
-  module = imp.load_module(type, f, filename, description)
-  try:
-    f, filename, description = imp.find_module(name, [filename])
-    module = imp.load_module(name, f, filename, description)
-    importedModules[type, name] = module
-    print("Successfully loaded {} module {} from {}".format(type, name, filename))
-  except ImportError as err:
-    print("Could not import {} module {} error {}".format(type, name, err))
+  if "{}.{}".format(type, name) in importedModules:
+    return importedModules["{}.{}".format(type, name)]
+  else:
+    f, filename, description = imp.find_module("modules")
+    module = imp.load_module("modules", f, filename, description)
+    f, filename, description = imp.find_module(type, [filename])
+    module = imp.load_module(type, f, filename, description)
+    try:
+      f, filename, description = imp.find_module(name, [filename])
+      module = imp.load_module(name, f, filename, description)
+      importedModules["{}.{}".format(type, name)] = module
+      print("Successfully loaded {} module {} from {}".format(type, name, filename))
+      return module
+    except ImportError as err:
+      print("Could not import {} module {} error {}".format(type, name, err))
+      return False
 
-def importModules():
-  for name, dict in config()["inputClasses"].items():
-    importModule("input", name)
-  for name, dict in config()["outputClasses"].items():
-    importModule("output", name)
+def runThreads():
+  for name, dict in config()["inputs"].items():
+    module = importModule("input", dict["className"])
+    assert module, "Input could not be loaded, not running thread {}".format(dict["className"])
+    if module:
+      class_ = getattr(module, dict["className"])
+      inputThreads[name] = class_()
+  for name, dict in config()["outputs"].items():
+    module = importModule("output", dict["className"])
+    assert module, "Output could not be loaded, not running thread {}".format(dict["className"])
+    if module:
+      class_ = getattr(module, dict["className"])
+      outputThreads[name] = class_()
   
 if __name__ == "__main__":
     # Ensure we exit cleanly
     signal.signal(signal.SIGINT, signal_handler)
-    importModules()
-    #start_inputThreads()
-    #start_outputThreads()
+    # Finally, we can instantiate the classes. This will kick off threads
+    runThreads()
     
